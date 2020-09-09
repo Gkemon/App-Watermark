f0aaa22915c9633e1461c2c006b0b467864d4bc1
Emon <emon@field.buzz>
2020-09-09 16:28:52 +0600

    -- Add log for debugger.

--- app/src/main/java/com/gk/emon/app/watermark/MainActivity.java
+++ app/src/main/java/com/gk/emon/app/watermark/MainActivity.java
@@ -2,34 +2,37 @@ package com.gk.emon.app.watermark;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    public static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         AppWaterMarkBuilder.doConfigure()
                 .setAppCompatActivity(MainActivity.this)
-                .setWatermarkProperty(R.layout.layout_water_mark)
+                .setWatermarkProperty(R.layout.layout_water_mark,20,R.color.colorAccent)
                 .showWatermarkAfterConfig(new WatermarkListener() {
                     @Override
                     public void onSuccess() {
-
+                        Log.d(TAG,"Successfully showing water mark");
                     }
 
                     @Override
                     public void onFailure(String message, Throwable throwable) {
-
+                        Log.d(TAG,"Failed: "+ message);
                     }
 
                     @Override
                     public void showLog(String log, @Nullable Throwable throwable) {
-
+                        Log.d(TAG,"Log: "+ log);
                     }
                 });
 

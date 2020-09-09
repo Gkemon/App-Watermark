d5fec9de094051b25df2226d1304b49f0cd55c37
Emon <emon@field.buzz>
2020-09-09 10:16:40 +0600

    Remove internal for error

--- App Watermark/src/main/java/com/gk/emon/app/watermark/WatermarkListener.kt
+++ App Watermark/src/main/java/com/gk/emon/app/watermark/WatermarkListener.kt
@@ -1,6 +1,6 @@
 package com.gk.emon.app.watermark
 
-internal interface WatermarkListener {
+ interface WatermarkListener {
     fun onSuccess()
     fun showLog(log: String?, throwable: Throwable?)
     fun onFailure(message: String?, throwable: Throwable?)
